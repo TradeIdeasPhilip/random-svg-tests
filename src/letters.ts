b39d7c749d5a16062627cc30150b8e7d1ac0f985
Philip Smolen <philip@trade-ideas.com>
2024-05-29 18:14:47 -0700

    Reorganize the code.
    Use more and better names for the constants.
    No changes are visible to the user.

--- src/letters.ts
+++ src/letters.ts
@@ -16,30 +16,65 @@ class FontMetrics {
   get xHeight() {
     return this.fontSize * 0.5;
   }
+  /**
+   * The width of a capital A.
+   */
   get aWidth() {
     return this.fontSize * 0.75;
   }
+  /**
+   * The width of a 0, 1, ..., or 9.
+   *
+   * A lot of other characters use this same width because they are designed the same way as the digits.
+   */
   get digitWidth() {
     return this.fontSize * 0.5;
   }
+  /**
+   * Put this much space between adjacent characters.
+   */
   get defaultKerning() {
     return this.fontSize * 0.25;
   }
-  get baseline() {
-    return 0;
-  }
+  /**
+   * The y coordinate for the top of most capital letters.
+   */
   get capitalTop() {
     return this.baseline - this.mHeight;
   }
-  get capitalMiddle() {
-    return this.baseline - this.xHeight;
-  }
+  /**
+   * Exactly half way between capitalTop and capitalMiddle.
+   */
   get capitalTopMiddle() {
     return (this.capitalTop + this.capitalMiddle) / 2;
   }
+  /**
+   * The y coordinate for the mid line for E, F, G, H, etc.
+   */
+  get capitalMiddle() {
+    return this.baseline - this.xHeight;
+  }
+  /**
+   * Exactly half way between capitalMiddle  and baseline.
+   */
   get capitalBottomMiddle() {
     return (this.baseline + this.capitalMiddle) / 2;
   }
+  /**
+   * The bottom of most letters, but not the descenders.
+   *
+   * The baseline is often used to line up different elements.
+   */
+  get baseline() {
+    return 0;
+  }
+
+  /**
+   *
+   * @param fontSize How tall to make the font.
+   * This is measured in svg units.
+   * This is typically (but not always) the same as the mHeight.
+   */
   constructor(public readonly fontSize: number) {
     if (fontSize <= 0 || !isFinite(fontSize)) {
       throw new Error("wtf");
@@ -55,12 +90,27 @@ class DescriptionOfLetter {
   constructor(
     public readonly letter: string,
     public readonly d: string,
+    /**
+     * How far to advance the print head after printing this character.
+     * In SVG units.
+     * Typically the "black" area of the character will start at x=0 and send at x=advance.
+     */
     public readonly advance: number,
     public readonly fontMetrics: FontMetrics
   ) {}
+  /**
+   * This is in the right format for a lot of _css properties_.
+   *
+   * If you are planning to set the d _attribute_ of an element, use this.d instead.
+   */
   get cssPath() {
     return cssifyPath(this.d);
   }
+  /**
+   * Create a new element to draw this letter.
+   * @returns Currently this is always a <path> element.
+   * I might or might not want to change that to be a <g> element.
+   */
   makeElement(): SVGElement {
     const pathElement = document.createElementNS(
       "http://www.w3.org/2000/svg",
@@ -90,7 +140,15 @@ function makeLineFont(fontSize: number) {
     );
     result.set(description.letter, description);
   };
-  const { aWidth, mHeight, xHeight, digitWidth } = fontMetrics;
+  const {
+    aWidth,
+    digitWidth,
+    capitalTop,
+    capitalTopMiddle,
+    capitalMiddle,
+    capitalBottomMiddle,
+    baseline,
+  } = fontMetrics;
   {
     // MARK: 0
     const advance = digitWidth;
@@ -98,9 +156,7 @@ function makeLineFont(fontSize: number) {
     const x1 = radius;
     const x2 = advance;
     const y1 = -radius;
-    const y2 = -(mHeight - radius);
-    const y3 = -mHeight;
-    const d = `M ${x1},${y3} Q ${x2},${y3} ${x2},${y2} L ${x2},${y1} Q ${x2},${fontMetrics.baseline} ${x1},${fontMetrics.baseline} Q 0,${fontMetrics.baseline} 0,${y1} L 0,${y2} Q 0,${y3} ${x1},${y3}`;
+    const d = `M ${x1},${capitalTop} Q ${x2},${capitalTop} ${x2},${capitalTopMiddle} L ${x2},${y1} Q ${x2},${baseline} ${x1},${baseline} Q 0,${baseline} 0,${y1} L 0,${capitalTopMiddle} Q 0,${capitalTop} ${x1},${capitalTop}`;
     add("0", d, advance);
   }
   {
@@ -109,14 +165,12 @@ function makeLineFont(fontSize: number) {
     const radius = advance / 2;
     const x1 = radius;
     const x2 = advance;
-    const y1 = -(mHeight - radius);
-    const y2 = -mHeight;
-    const d = `M 0,${y1} Q ${x1},${y1} ${x1},${y2} L ${x1},${fontMetrics.baseline} M 0,${fontMetrics.baseline} L ${x2},${fontMetrics.baseline}`;
+    const d = `M 0,${capitalTopMiddle} Q ${x1},${capitalTopMiddle} ${x1},${capitalTop} L ${x1},${baseline} M 0,${baseline} L ${x2},${baseline}`;
     add("1", d, advance);
   }
   {
     // MARK: 2
-    //const advance = mHeight / 2;
+    //const advance = digitWidth;
     //const radius = advance / 2;
   }
   {
@@ -128,8 +182,7 @@ function makeLineFont(fontSize: number) {
     const y1 = -radius;
     const y2 = -2 * radius;
     const y3 = -3 * radius;
-    const y4 = -mHeight;
-    const d = `M ${x1},${y4} Q ${x2},${y4} ${x2},${y3} Q ${x2},${y2} ${x1},${y2} Q 0,${y2} 0,${y1} Q 0,${fontMetrics.baseline} ${x1},${fontMetrics.baseline} Q ${x2},${fontMetrics.baseline} ${x2},${y1} Q ${x2},${y2} ${x1},${y2} Q 0,${y2} 0,${y3} Q 0,${y4} ${x1},${y4}`;
+    const d = `M ${x1},${capitalTop} Q ${x2},${capitalTop} ${x2},${y3} Q ${x2},${y2} ${x1},${y2} Q 0,${y2} 0,${y1} Q 0,${baseline} ${x1},${baseline} Q ${x2},${baseline} ${x2},${y1} Q ${x2},${y2} ${x1},${y2} Q 0,${y2} 0,${y3} Q 0,${capitalTop} ${x1},${capitalTop}`;
     add("8", d, advance);
   }
   {
@@ -141,8 +194,7 @@ function makeLineFont(fontSize: number) {
     //const y1 = -radius;
     const y2 = -2 * radius;
     const y3 = -3 * radius;
-    const y4 = -mHeight;
-    const d = `M ${x2}, ${y3} Q ${x2},${y4} ${x1},${y4} Q 0,${y4} 0,${y3} Q 0,${y2} ${x1},${y2} Q ${x2},${y2} ${x2},${y3} Q ${x2},${fontMetrics.baseline} 0,${fontMetrics.baseline}`;
+    const d = `M ${x2}, ${y3} Q ${x2},${capitalTop} ${x1},${capitalTop} Q 0,${capitalTop} 0,${y3} Q 0,${y2} ${x1},${y2} Q ${x2},${y2} ${x2},${y3} Q ${x2},${baseline} 0,${baseline}`;
     add("9", d, advance);
   }
   {
@@ -152,8 +204,7 @@ function makeLineFont(fontSize: number) {
     const x2 = advance;
     const y1 = -2 * radius;
     const y2 = -3 * radius;
-    const y3 = -mHeight;
-    const d = `M ${x2}, ${y2} Q ${x2},${y3} ${x1},${y3} Q 0,${y3} 0,${y2} Q 0,${y1} ${x1},${y1} Q ${x2},${y1} ${x2},${y2} L ${x2},${fontMetrics.baseline}`;
+    const d = `M ${x2}, ${y2} Q ${x2},${capitalTop} ${x1},${capitalTop} Q 0,${capitalTop} 0,${y2} Q 0,${y1} ${x1},${y1} Q ${x2},${y1} ${x2},${y2} L ${x2},${baseline}`;
     add("9a", d, advance);
   }
   {
@@ -164,8 +215,7 @@ function makeLineFont(fontSize: number) {
     const y1 = -radius;
     const y2 = -2 * radius;
     const y3 = -3 * radius;
-    const y4 = -mHeight;
-    const d = `M ${x2}, ${y3} Q ${x2},${y4} ${x1},${y4} Q 0,${y4} 0,${y3} Q 0,${y2} ${x1},${y2} Q ${x2},${y2} ${x2},${y3} L ${x2},${y1} Q ${x2},${fontMetrics.baseline} ${x1},${fontMetrics.baseline} Q 0,${fontMetrics.baseline} 0,${y1}`;
+    const d = `M ${x2}, ${y3} Q ${x2},${capitalTop} ${x1},${capitalTop} Q 0,${capitalTop} 0,${y3} Q 0,${y2} ${x1},${y2} Q ${x2},${y2} ${x2},${y3} L ${x2},${y1} Q ${x2},${baseline} ${x1},${baseline} Q 0,${baseline} 0,${y1}`;
     add("9b", d, advance);
   }
   {
@@ -176,136 +226,112 @@ function makeLineFont(fontSize: number) {
     const y1 = -radius;
     const y2 = -2 * radius;
     const y3 = -3 * radius;
-    const y4 = -mHeight;
-    const d = `M ${x2}, ${y3} Q ${x2},${y4} ${x1},${y4} Q 0,${y4} 0,${y3} Q 0,${y2} ${x1},${y2} Q ${x2},${y2} ${x2},${y3} Q ${x2},${fontMetrics.baseline} ${x1},${fontMetrics.baseline} Q 0,${fontMetrics.baseline} 0,${y1}`;
+    const d = `M ${x2}, ${y3} Q ${x2},${capitalTop} ${x1},${capitalTop} Q 0,${capitalTop} 0,${y3} Q 0,${y2} ${x1},${y2} Q ${x2},${y2} ${x2},${y3} Q ${x2},${baseline} ${x1},${baseline} Q 0,${baseline} 0,${y1}`;
     add("9c", d, advance);
   }
   {
     // MARK: A
-    const d = `M 0,${fontMetrics.baseline} L ${fontMetrics.aWidth / 2},${
-      fontMetrics.baseline - mHeight
-    } L ${fontMetrics.aWidth},${fontMetrics.baseline} M ${
-      fontMetrics.aWidth / 4
-    },${fontMetrics.baseline - xHeight} L ${fontMetrics.aWidth * 0.75},${
-      fontMetrics.baseline - xHeight
-    }`;
+    const d = `M 0,${baseline} L ${fontMetrics.aWidth / 2},${capitalTop} L ${
+      fontMetrics.aWidth
+    },${baseline} M ${fontMetrics.aWidth / 4},${capitalMiddle} L ${
+      fontMetrics.aWidth * 0.75
+    },${capitalMiddle}`;
     add("A", d, aWidth);
   }
   {
     // MARK: B
-    const advance = mHeight / 2;
-    const topRadius = (mHeight - xHeight) / 2;
+    const advance = digitWidth;
+    const topRadius = capitalTopMiddle - capitalTop;
+    if (topRadius <= 0) {
+      throw new Error("wtf");
+    }
     const topLineLength = (advance - topRadius) * (2 / 3);
-    const bottomRadius = xHeight / 2;
+    const bottomRadius = baseline - capitalBottomMiddle;
+    if (bottomRadius <= 0) {
+      throw new Error("wtf");
+    }
     const bottomLineLength = advance - bottomRadius;
-    const d = `M 0,${fontMetrics.baseline - mHeight} L 0,${
-      fontMetrics.baseline
-    } L ${bottomLineLength},${fontMetrics.baseline} M ${Math.max(
+    const d = `M 0,${capitalTop} L 0,${baseline} L ${bottomLineLength},${baseline} M ${Math.max(
       bottomLineLength,
       topLineLength
-    )},${fontMetrics.baseline - xHeight} L 0,${
-      fontMetrics.baseline - xHeight
-    } M 0,${fontMetrics.baseline - mHeight} L ${topLineLength},${
-      fontMetrics.baseline - mHeight
-    } Q ${topLineLength + topRadius},${fontMetrics.baseline - mHeight} ${
+    )},${capitalMiddle} L 0,${capitalMiddle} M 0,${capitalTop} L ${topLineLength},${capitalTop} Q ${
       topLineLength + topRadius
-    },${fontMetrics.baseline - mHeight + topRadius} Q ${
+    },${capitalTop} ${topLineLength + topRadius},${capitalTop + topRadius} Q ${
       topLineLength + topRadius
-    },${fontMetrics.baseline - xHeight} ${topLineLength},${
-      fontMetrics.baseline - xHeight
-    } M ${bottomLineLength},${fontMetrics.baseline - xHeight} Q ${
+    },${capitalMiddle} ${topLineLength},${capitalMiddle} M ${bottomLineLength},${capitalMiddle} Q ${
       bottomLineLength + bottomRadius
-    },${fontMetrics.baseline - xHeight} ${bottomLineLength + bottomRadius},${
-      fontMetrics.baseline - bottomRadius
-    } Q ${bottomLineLength + bottomRadius},${
-      fontMetrics.baseline
-    } ${bottomLineLength},${fontMetrics.baseline}`;
+    },${capitalMiddle} ${bottomLineLength + bottomRadius},${
+      baseline - bottomRadius
+    } Q ${
+      bottomLineLength + bottomRadius
+    },${baseline} ${bottomLineLength},${baseline}`;
     add("B", d, advance);
   }
   {
     // MARK: C
-    const advance = Math.min(aWidth, mHeight * 0.5);
+    const advance = digitWidth;
     const radius = advance / 2;
     const x1 = radius;
     const x2 = advance;
-    const y1 = fontMetrics.baseline - radius;
-    const y3 = fontMetrics.baseline - mHeight;
-    const y2 = y3 + radius;
-    const d = `M ${x2},${y2} Q ${x2},${y3} ${x1},${y3} Q 0,${y3} 0,${y2} L 0,${y1} Q 0,${fontMetrics.baseline} ${x1},${fontMetrics.baseline} Q ${x2},${fontMetrics.baseline} ${x2},${y1}`;
+    const d = `M ${x2},${capitalTopMiddle} Q ${x2},${capitalTop} ${x1},${capitalTop} Q 0,${capitalTop} 0,${capitalTopMiddle} L 0,${capitalBottomMiddle} Q 0,${baseline} ${x1},${baseline} Q ${x2},${baseline} ${x2},${capitalBottomMiddle}`;
     add("C", d, advance);
   }
   {
     // MARK: D
-    const advance = Math.min(aWidth, mHeight * 0.5);
+    const advance = digitWidth;
     const radius = advance / 2;
     const x1 = radius;
     const x2 = advance;
-    const y1 = fontMetrics.baseline - radius;
-    const y3 = fontMetrics.baseline - mHeight;
-    const y2 = y3 + radius;
-    const d = `M 0,${y3} L 0,${fontMetrics.baseline} L ${x1},${fontMetrics.baseline} Q ${x2},${fontMetrics.baseline} ${x2},${y1} L ${x2},${y2} Q ${x2},${y3} ${x1},${y3} L 0,${y3}`;
+    const d = `M 0,${capitalTop} L 0,${baseline} L ${x1},${baseline} Q ${x2},${baseline} ${x2},${capitalBottomMiddle} L ${x2},${capitalTopMiddle} Q ${x2},${capitalTop} ${x1},${capitalTop} L 0,${capitalTop}`;
     add("D", d, advance);
   }
   {
     // MARK: E
-    const advance = mHeight / 2;
+    const advance = digitWidth;
     const x1 = advance * (2 / 3);
     const x2 = advance;
-    const y1 = fontMetrics.baseline - xHeight;
-    const y2 = fontMetrics.baseline - mHeight;
-    const d = `M ${x2},${y2} L 0,${y2} L 0,${fontMetrics.baseline} L ${x2},${fontMetrics.baseline} M ${x1},${y1} L 0,${y1}`;
+    const d = `M ${x2},${capitalTop} L 0,${capitalTop} L 0,${baseline} L ${x2},${baseline} M ${x1},${capitalMiddle} L 0,${capitalMiddle}`;
     add("E", d, advance);
   }
   {
     // MARK: F
-    const advance = mHeight / 2;
+    const advance = digitWidth;
     const x1 = advance * (2 / 3);
     const x2 = advance;
-    const y1 = fontMetrics.baseline - xHeight;
-    const y2 = fontMetrics.baseline - mHeight;
-    const d = `M ${x2},${y2} L 0,${y2} L 0,${fontMetrics.baseline} M ${x1},${y1} L 0,${y1}`;
+    const d = `M ${x2},${capitalTop} L 0,${capitalTop} L 0,${baseline} M ${x1},${capitalMiddle} L 0,${capitalMiddle}`;
     add("F", d, advance);
   }
   {
     // MARK: G
-    const advance = mHeight * 0.5;
+    const advance = digitWidth;
     const radius = advance / 2;
     const x1 = radius;
     const x2 = advance;
-    const y1 = fontMetrics.baseline - radius;
-    const y2 = fontMetrics.baseline - xHeight;
-    const y4 = fontMetrics.baseline - mHeight;
-    const y3 = y4 + radius;
-    const d = `M ${x2},${y3} Q ${x2},${y4} ${x1},${y4} Q 0,${y4} 0,${y3} L 0,${y1} Q 0,${fontMetrics.baseline} ${x1},${fontMetrics.baseline} Q ${x2},${fontMetrics.baseline} ${x2},${y1} L ${x2},${y2} L ${x1},${y2}`;
+    const d = `M ${x2},${capitalTopMiddle} Q ${x2},${capitalTop} ${x1},${capitalTop} Q 0,${capitalTop} 0,${capitalTopMiddle} L 0,${capitalBottomMiddle} Q 0,${baseline} ${x1},${baseline} Q ${x2},${baseline} ${x2},${capitalBottomMiddle} L ${x2},${capitalMiddle} L ${x1},${capitalMiddle}`;
     add("G", d, advance);
   }
   {
     // MARK: H
-    const advance = mHeight / 2;
+    const advance = digitWidth;
     const x1 = advance;
-    const y1 = fontMetrics.baseline - xHeight;
-    const y2 = fontMetrics.baseline - mHeight;
-    const d = `M 0,${y2} L 0,${fontMetrics.baseline} M ${x1},${y2} L ${x1},${fontMetrics.baseline} M 0,${y1} L ${x1},${y1}`;
+    const d = `M 0,${capitalTop} L 0,${baseline} M ${x1},${capitalTop} L ${x1},${baseline} M 0,${capitalMiddle} L ${x1},${capitalMiddle}`;
     add("H", d, advance);
   }
   {
     // MARK: I
-    const advance = mHeight / 3;
+    const advance = fontMetrics.mHeight / 3;
     const x1 = advance / 2;
     const x2 = advance;
-    const y1 = fontMetrics.baseline - mHeight;
-    const d = `M 0,${y1} L ${x2},${y1} M 0,${fontMetrics.baseline} L ${x2},${fontMetrics.baseline} M ${x1},${y1} L ${x1},${fontMetrics.baseline}`;
+    const d = `M 0,${capitalTop} L ${x2},${capitalTop} M 0,${baseline} L ${x2},${baseline} M ${x1},${capitalTop} L ${x1},${baseline}`;
     add("I", d, advance);
   }
   {
     // MARK: J
-    const advance = mHeight / 2;
+    const advance = digitWidth;
     const radius = advance / 2;
     const x1 = radius;
     const x2 = advance;
-    const y1 = fontMetrics.baseline - radius;
-    const y2 = fontMetrics.baseline - mHeight;
-    const d = `M ${x2},${y2} L ${x2},${y1} Q ${x2},${fontMetrics.baseline} ${x1},${fontMetrics.baseline} Q 0,${fontMetrics.baseline} 0,${y1}`;
+    const d = `M ${x2},${capitalTop} L ${x2},${capitalBottomMiddle} Q ${x2},${baseline} ${x1},${baseline} Q 0,${baseline} 0,${capitalBottomMiddle}`;
     add("J", d, advance);
   }
   return result;
@@ -344,7 +370,8 @@ function makeLineFont(fontSize: number) {
     }
   }
 
-  show("");
+  show(""); // Show something specific or
+  // Show everything that's available.
   for (const char of font.keys()) {
     show1(char);
   }

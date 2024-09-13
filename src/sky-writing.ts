cb5b93239729a1fdda2d03c4f14f806e89d34153
Philip Smolen <philip@trade-ideas.com>
2024-09-13 16:14:09 -0700

    Implement the "Simple" checkbox.
    For now the "Handwriting" checkbox just clears the screen.
    That's enough to test the other checkbox!
    
    Fix the layout of the configuration area.
    I think I broke that when I centered the <body> recently.
    Add a border around that area in part for debugging.

--- src/sky-writing.ts
+++ src/sky-writing.ts
@@ -15,17 +15,73 @@ const mainSvg = getById("main", SVGElement);
   span.innerText = [...normal].sort().join(" ");
 }
 
-function updateSvg() {
-  mainSvg.innerHTML = "";
-  const textLayout = new TextLayout();
-  textLayout.restart();
-  const text = inputTextArea.value;
-  const t = textLayout.addText(text);
-  textLayout.displayText(t, mainSvg);
-  mainSvg.ownerSVGElement!.viewBox.baseVal.height =
-    textLayout.baseline + textLayout.font.get("0")!.fontMetrics.bottom;
+abstract class AnimationController {
+  static #current: undefined | AnimationController; // (() => void);
+  /**
+   * Stop the current animation (if there is one) and clear the SVG.
+   */
+  static doCleanup() {
+    this.#current?.doCleanup();
+    this.#current = undefined;
+    mainSvg.innerHTML = "";
+  }
+  /**
+   * Does nothing if there is no current animation.
+   */
+  static restartCurrent() {
+    const current = this.#current;
+    if (current) {
+      this.doCleanup();
+      current.start();
+    }
+  }
+  start() {
+    AnimationController.doCleanup();
+    this.startImpl();
+    AnimationController.#current = this;
+  }
+  protected abstract startImpl(): void;
+  protected doCleanup() {}
+}
+
+class Simple extends AnimationController {
+  override startImpl(): void {
+    const textLayout = new TextLayout();
+    textLayout.restart();
+    const text = inputTextArea.value;
+    const t = textLayout.addText(text);
+    textLayout.displayText(t, mainSvg);
+    mainSvg.ownerSVGElement!.viewBox.baseVal.height =
+      textLayout.baseline + textLayout.font.get("0")!.fontMetrics.bottom;
+  }
+  static start() {
+    new this().startImpl();
+  }
 }
 
 inputTextArea.addEventListener("input", () => {
-  updateSvg();
+  AnimationController.restartCurrent();
+});
+
+function selectAnimation() {
+  const id = document.querySelector('input[name="type"]:checked')?.id;
+  console.log(id);
+  switch (id) {
+    case "simpleType": {
+      new Simple().start();
+      break;
+    }
+    case "handwritingType": {
+      AnimationController.doCleanup();
+      break;
+    }
+    default: {
+      throw new Error(`invalid id: ${id}`);
+    }
+  }
+}
+
+selectAnimation();
+document.querySelectorAll('input[name="type"]').forEach((element) => {
+  element.addEventListener("click", selectAnimation);
 });

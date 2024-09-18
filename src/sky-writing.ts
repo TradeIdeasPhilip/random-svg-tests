705273bad810485ce92f33fda7491bcb3af5c7de
Philip Smolen <philip@trade-ideas.com>
2024-09-18 18:11:21 -0500

    Get rid of the white dots
    The handwriting effect was leaving little dots everywhere.
    
    And adjust some of the margins.
    Those were broken when I changed some div's display from the default to

--- src/sky-writing.ts
+++ src/sky-writing.ts
@@ -70,35 +70,40 @@ class Handwriting extends AnimationController {
     mainSvg.ownerSVGElement!.viewBox.baseVal.height =
       textLayout.baseline + textLayout.font.get("0")!.fontMetrics.bottom;
     const pathShape = TextLayout.join(t);
-    const parentGElement = document.createElementNS("http://www.w3.org/2000/svg","g");
+    const parentGElement = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "g"
+    );
     parentGElement.classList.add("handwriting");
     mainSvg.appendChild(parentGElement);
     let soFar = 0;
-    pathShape.splitOnMove().forEach(shape => {
-      const element = document.createElementNS("http://www.w3.org/2000/svg","path");
+    pathShape.splitOnMove().forEach((shape) => {
+      const element = document.createElementNS(
+        "http://www.w3.org/2000/svg",
+        "path"
+      );
       element.setAttribute("d", shape.rawPath);
       parentGElement.appendChild(element);
       const before = soFar;
       const length = element.getTotalLength();
       const after = before + length;
       soFar = after;
-      element.style.setProperty("--offset",before.toString());
-      element.style.setProperty("--length",length.toString());
+      element.style.setProperty("--offset", before.toString());
+      element.style.setProperty("--length", length.toString());
     });
     const abortController = new AbortController();
     this.doCleanup = () => abortController.abort();
-    // finish one time stuff. 
+    // finish one time stuff.
     const totalLength = soFar;
     const updatePosition = () => {
-      const position =  completionRatioInput.valueAsNumber * totalLength;
+      const position = completionRatioInput.valueAsNumber * totalLength;
       assertFinite(position);
-      parentGElement.style.setProperty("--total-position",position.toString());
+      parentGElement.style.setProperty("--total-position", position.toString());
     };
     updatePosition();
     completionRatioInput.addEventListener("input", updatePosition, {
       signal: abortController.signal,
     });
-    //    throw new Error("Method not implemented."); TODO finishme
   }
   static start() {
     new this().start();

ab362cddab5cc199fae6387130230942ca3258d0
Philip Smolen <philip@trade-ideas.com>
2024-10-02 21:04:36 -0700

    Add more check boxes.
    "Handwriting" now refers to the original handwriting demo.
    "Skywriting" now refers to the new skywriting demo.
    In the previous release the "Handwriting" radio button displayed the skywriting demo!

--- src/sky-writing.ts
+++ src/sky-writing.ts
@@ -94,6 +94,55 @@ class Handwriting extends AnimationController {
     const abortController = new AbortController();
     this.doCleanup = () => abortController.abort();
     // finish one time stuff.
+    const totalLength = soFar;
+    const updatePosition = () => {
+      const position = completionRatioInput.valueAsNumber * totalLength;
+      assertFinite(position);
+      parentGElement.style.setProperty("--total-position", position.toString());
+    };
+    updatePosition();
+    completionRatioInput.addEventListener("input", updatePosition, {
+      signal: abortController.signal,
+    });
+    //    throw new Error("Method not implemented."); TODO finishme
+  }
+  static start() {
+    new this().start();
+  }
+}
+class Skywriting extends AnimationController {
+  protected override startImpl(): void {
+    const textLayout = new TextLayout();
+    textLayout.restart();
+    const text = inputTextArea.value;
+    const t = textLayout.addText(text);
+    mainSvg.ownerSVGElement!.viewBox.baseVal.height =
+      textLayout.baseline + textLayout.font.get("0")!.fontMetrics.bottom;
+    const pathShape = TextLayout.join(t);
+    const parentGElement = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "g"
+    );
+    parentGElement.classList.add("handwriting", "skywriting");
+    mainSvg.appendChild(parentGElement);
+    let soFar = 0;
+    pathShape.splitOnMove().forEach((shape) => {
+      const element = document.createElementNS(
+        "http://www.w3.org/2000/svg",
+        "path"
+      );
+      element.setAttribute("d", shape.rawPath);
+      parentGElement.appendChild(element);
+      const before = soFar;
+      const length = element.getTotalLength();
+      const after = before + length;
+      soFar = after;
+      element.style.setProperty("--offset", before.toString());
+      element.style.setProperty("--length", length.toString());
+    });
+    const abortController = new AbortController();
+    this.doCleanup = () => abortController.abort();
+    // finish one time stuff.
     /**
      * Continue for enough time to add this much length.
      * Nothing will be added but what's there will be animated.
@@ -128,6 +177,14 @@ function selectAnimation() {
     }
     case "handwritingType": {
       new Handwriting().start();
+      break;
+    }
+    case "skywritingType": {
+      new Skywriting().start();
+      break;
+    }
+    case "roughType": {
+      new Skywriting().start();
       break;
     }
     default: {

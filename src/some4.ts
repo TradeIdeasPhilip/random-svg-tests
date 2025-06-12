02314191708e606aab44dffd9cba6b4a52db9eac
Philip Smolen <philip@trade-ideas.com>
2025-06-12 10:30:12 -0700

    Add script for part 4
    This is similar to part 1, but with the bad points display turned on.
    For this example it only shows the real line and the centers of the 3 taylor series.

--- src/some4.ts
+++ src/some4.ts
@@ -207,9 +207,14 @@ const variableBase = document.documentElement.style;
  */
 class TaylorElements {
   readonly #center: SVGCircleElement;
+  readonly #bottomCenter: SVGCircleElement;
   readonly #path: SVGPathElement;
   constructor(readonly which: string) {
     this.#center = selectorQuery(`[data-center="${which}"]`, SVGCircleElement);
+    this.#bottomCenter = selectorQuery(
+      `[data-circle-info="${which}"] [data-bottom-center]`,
+      SVGCircleElement
+    );
     this.#path = selectorQuery(
       `[data-reconstruction="${which}"]`,
       SVGPathElement
@@ -218,6 +223,7 @@ class TaylorElements {
   hide() {
     variableBase.setProperty(`--example${this.which}-open-end-display`, "none");
     this.#center.style.display = "none";
+    this.#bottomCenter.style.display = "none";
     this.#path.style.d = "";
   }
   draw2(
@@ -260,6 +266,7 @@ class TaylorElements {
     const shape = PathShape.parametric(p, 50);
     this.#path.style.d = shape.cssPath;
     this.#center.style.display = "";
+    this.#bottomCenter.style.display = "";
     variableBase.setProperty(`--example${this.which}-center-x`, center + "px");
     variableBase.setProperty(
       `--example${this.which}-center-y`,
@@ -638,6 +645,66 @@ class ScriptDispatcher {
         termsToShow
       );
     }
+  }
+
+  function initScreenCapture(): {
+    firstFrame: number;
+    lastFrame: number;
+  } {
+    return {
+      firstFrame: 0,
+      lastFrame: Math.floor((END_TIME / 1000) * 60), // Convert from milliseconds to the number of frames.
+    };
+  }
+  ScriptDispatcher.instance.addScript(scriptName, { show, initScreenCapture });
+}
+
+{
+  const scriptName = "part 4";
+
+  // Goal 23 seconds.
+  const MAX_NUMBER_OF_TERMS = 23;
+  const HOLD_TIME = 500;
+  const TRANSITION_TIME = 478.5;
+  const END_TIME =
+    MAX_NUMBER_OF_TERMS * (TRANSITION_TIME + HOLD_TIME) + HOLD_TIME;
+
+  console.log(`${scriptName} length: ${END_TIME / 1000} seconds.`);
+
+  /**
+   * Very similar to part 1 but with the radius of convergence showing.
+   * @param time Measured in frames.
+   */
+  function show(time: DOMHighResTimeStamp): void {
+    if (time < 0) {
+      // TODO hide everything??
+      return;
+    }
+    if (time >= END_TIME) {
+      // TODO hide everything??
+      return;
+    }
+    const integerNumberOfTerms = Math.floor(
+      time / (HOLD_TIME + TRANSITION_TIME)
+    );
+    const timeWithinTerm =
+      time - integerNumberOfTerms * (HOLD_TIME + TRANSITION_TIME);
+    const timeWithinTransition = Math.max(0, timeWithinTerm - HOLD_TIME);
+    const fraction = timeWithinTransition / TRANSITION_TIME;
+    const termsToShow = integerNumberOfTerms + fraction;
+    const functionInfo = Sine.instance;
+    OriginalFunctionElement.instance.draw(functionInfo, true);
+    TaylorElements.instances[0].drawAll(
+      functionInfo,
+      (-Math.PI * 7) / 4,
+      termsToShow
+    );
+    TaylorElements.instances[1].drawAll(functionInfo, 0, termsToShow);
+    TaylorElements.instances[2].drawAll(
+      functionInfo,
+      (Math.PI * 3) / 2,
+      termsToShow
+    );
   }
 
   function initScreenCapture(): {
